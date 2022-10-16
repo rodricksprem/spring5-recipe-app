b0e5871809e3d89a66736cc52a43fa47c3415771
rodricks <rodricks.premkumar@biogen.com>
2022-10-16 18:14:02 +0530

    Initialize a Database Using Basic SQL Scripts

--- src/main/java/guru/springframework/spring5recipeapp/domains/Category.java
+++ src/main/java/guru/springframework/spring5recipeapp/domains/Category.java
@@ -1,16 +1,16 @@
 package guru.springframework.spring5recipeapp.domains;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String categoryName;
+    private String description;
+
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
@@ -22,12 +22,12 @@ public class Category {
         this.id = id;
     }
 
-    public String getCategoryName() {
-        return categoryName;
+    public String getDescription() {
+        return description;
     }
 
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Set<Recipe> getRecipes() {
