b9219f0cc55112525836095d2fdf9c78c502983b
rodricks <rodricks.premkumar@biogen.com>
2022-10-16 07:19:07 +0530

    add index page

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping({"","/","index"})
     public String getIndex(){
+        System.out.println("index is present here");
         return "index";
     }
 }
