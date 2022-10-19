30c7c26430fc03a97e3be2ab1ba60e1ee8f3dc99
rodricks <rodricks.premkumar@biogen.com>
2022-10-20 05:10:47 +0530

    using-mockito-mocks

---
+++ src/test/java/guru/springframework/spring5recipeapp/service/RecipeServiceImplTest.java
@@ -0,0 +1,35 @@
+package guru.springframework.spring5recipeapp.service;
+
+import guru.springframework.spring5recipeapp.domains.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+    RecipeServiceImpl recipeService;
+    @Mock
+     RecipeRepository recipeRepository;
+@BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+        recipeService=new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipies() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipes);
+        assertEquals(1,recipeService.getRecipies().size());
+        verify(recipeRepository,times(1)).findAll();
+    }
+}
