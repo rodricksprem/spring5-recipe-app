ebfe592055791ac8635d03e944c43b277c316268
rodricks <rodricks.premkumar@biogen.com>
2022-10-20 11:32:42 +0530

    added mockiot-argument-capture

--- src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.spring5recipeapp.domains.Recipe;
 import guru.springframework.spring5recipeapp.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -31,9 +32,21 @@ class IndexControllerTest {
 
     @Test
     void getIndex() {
+        Set<Recipe> recipes = new HashSet<>();
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+        recipe.setId(2L);
+        recipe = new Recipe();
+        recipes.add(recipe);
+        when(recipeService.getRecipies()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
         String viewName=indexController.getIndex(model);
         assertEquals("index",viewName);
         verify(recipeService,times(1)).getRecipies();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> returnedSet= argumentCaptor.getValue();
+        assertEquals(2,returnedSet.size());
     }
 }
