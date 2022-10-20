74fa9e52d5f26db978efac9f07eab38baa852d15
rodricks <rodricks.premkumar@biogen.com>
2022-10-20 12:45:54 +0530

    spring-integration-test

---
+++ src/test/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring5recipeapp.repositories;
+
+import guru.springframework.spring5recipeapp.domains.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DataJpaTest
+class UnitOfMeasureRepositoryTest {
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon",unitOfMeasureOptional.get());
+    }
+}
