9a7c538c39e2e2211ae13553259ef642bfbfef9f
rodricks <rodricks.premkumar@biogen.com>
2022-10-21 19:47:48 +0530

    assignment-review-display-all-of-recipe

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -24,6 +24,7 @@ public class IndexController {
     @RequestMapping({"","/","index"})
     public String getIndex(Model model){
         log.debug("inside getIndex method...");
+
         model.addAttribute("recipes",recipeService.getRecipies());
 
         return "index";
